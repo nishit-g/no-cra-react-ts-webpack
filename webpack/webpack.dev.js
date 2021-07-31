1a2109fb9c0060bdee1e1fa8673bb124ff8dee72
Nishit Gupta <nishit.gupta6708@gmail.com>
2021-07-31 16:42:19 +0530

    update start script + webpack.dev config"

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -3,6 +3,7 @@ module.exports = {
   mode: 'development',
   devServer: {
     hot: true,
+    open: true,
   },
   devtool: 'cheap-module-source-map',
   plugins: [new ReactRefreshWebpackPlugin()],
