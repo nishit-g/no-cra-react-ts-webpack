ffde23db8c584ae6671ed9f27a98ece3f9b68c88
Nishit Gupta <nishit.gupta6708@gmail.com>
2021-07-31 15:22:12 +0530

    use image files in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import './styles.css';
+import Eyes from './eyes.png';
 
 const App = () => {
-  return <h1>Is it working yet ?</h1>;
+  return (
+    <>
+      <h1>Is it working yet ?</h1>
+      <img src={Eyes} alt='Eyes' />
+    </>
+  );
 };
 
 export default App;
